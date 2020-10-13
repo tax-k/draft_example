bda302ef5bd9d06e26b68b3bb2bf849599b79f84
modu <taxfree9090@gmail.com>
2020-10-13 10:09:38 +0900

    first init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Editor, EditorState, RichUtils, convertToRaw } from 'draft-js';
+import 'draft-js/dist/Draft.css';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { editorState: EditorState.createEmpty() };
+
+		this.handleKeyCommand = this.handleKeyCommand.bind(this);
+	}
+
+	onChange = (editorState) => {
+		this.setState({ editorState });
+		const contentState = editorState.getCurrentContent();
+    console.log('content state', convertToRaw(contentState));
+    console.log('')
+	};
+
+	_onBoldClick() {
+		this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+	}
+
+	handleKeyCommand(command, editorState) {
+		const newState = RichUtils.handleKeyCommand(editorState, command);
+
+		if (newState) {
+			this.onChange(newState);
+			return 'handled';
+		}
+
+		return 'not-handled';
+	}
+
+	render() {
+		return (
+			<div>
+				<button onClick={this._onBoldClick.bind(this)}>Bold</button>
+				<Editor
+					editorState={this.state.editorState}
+					handleKeyCommand={this.handleKeyCommand}
+					onChange={this.onChange}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
